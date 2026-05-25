e77afea27f06f38166b208aefda5d63461d46e14
DSM1 <fatec-dsm1@academico.intra>
2026-05-25 19:32:35 -0300

    Operadores em Javascript

---
+++ aula-10-introducao-ao-javascript/02-operadores/script.js
@@ -0,0 +1,46 @@
+// Javascript
+
+// let n1 = Number(window.prompt('Digite um número: '))
+// let n2 = Number(window.prompt('Digite outro número: '))
+
+// const soma = n1 + n2
+
+// window.alert(`A soma dos valores é ${soma}`)
+
+// // Demais Operadores
+
+// // MULTIPLICAÇÃO
+// let mult = n1 * n2
+// document.writeln(`<br> A multiplicação de ${n1} e ${n2} é igual a ${mult}.`)
+
+// // SUBTRAÇÃO
+// let subt = n1 - n2
+// document.writeln(`<br> A subtração de ${n1} e ${n2} é igual a ${subt}.`)
+
+// // POTÊNCIA
+// let pot = n1 * n2
+// document.writeln(`<br> A potência de ${n1} por ${n2} é igual a ${pot}.`)
+
+// // DIVISÃO REAL
+// let divReal = n1 / n2
+// document.writeln(`<br> A divisão de ${n1} e ${n2} é igual a ${divReal}.`)
+
+// // RESTO DA DIVISÃO
+// let resto = n1 % n2
+// document.writeln(`<br> O resto da divisão de ${n1} e ${n2} é igual a ${resto}.`)
+
+// // OPERADOR DE ATRIBUIÇÃO
+// n1 += n2
+// document.writeln(`<br>${n1}`)
+// n1 += n2
+// document.writeln(`<br>${n1}`)
+// n1 += n2
+// document.writeln(`<br>${n1}`)
+
+let idade = Number(window.prompt(`Digite sua idade: `))
+
+if (idade >= 18){
+    document.writeln(`<br>Você é maior de idade`)
+}else{
+    document.writeln(`<br>Você é menor de idade`)
+}
